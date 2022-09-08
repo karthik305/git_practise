74c30b7b9680bd756a0e62f0e5265458a4e326d3
karthik305 <gkarthikraju3@gmail.com>
2022-09-08 22:46:06 +0530

    first git commit from cypress repo

---
+++ cypress.config.js
@@ -0,0 +1,26 @@
+const { defineConfig } = require('cypress')
+const cucumber = require('cypress-cucumber-preprocessor').default
+
+
+module.exports = defineConfig({
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 8000,
+  pageLoadTimeout: 30000,
+  reporter: 'mochawesome',
+  env: {
+    url: 'https://rahulshettyacademy.com',
+  },
+  retries: {
+    runMode: 1,
+  },
+  projectId: 'c7e5ev',
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      on('file:preprocessor', cucumber())
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    specPattern: 'cypress/integration/**/*.feature',
+  },
+})
